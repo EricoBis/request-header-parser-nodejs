6962238954b64bb0d201e8dc4371e087f6735534
Érico Bis <ericobis.santos@gmail.com>
2024-02-17 00:12:33 -0300

    add | GET endpoint api/whoami

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ const app = express();
 const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.get("/api/", (req, res) => {});
+app.get("/api/whoami", (req, res) => {
+  let ipaddress =
+    req.headers["x-forwarded-for"] ||
+    req.socket.remoteAddress;
+
+  // Converts the IPv6 address to IPv4, if applicable  
+  if (ipaddress.substr(0, 7) === "::ffff:") {
+    ipaddress = ipaddress.substr(7);
+  }
+
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
+});
 
 // listen for requests
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${process.env.PORT || 3000}`);
 });
